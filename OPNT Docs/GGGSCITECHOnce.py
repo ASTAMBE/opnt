149c873046279100ee757925191708668860b456
ASTAMBE <ast.common@gmail.com>
2023-08-27 19:11:14 -0700

    feed scraper

---
+++ OPNT Docs/GGGSCITECHOnce.py
@@ -0,0 +1,95 @@
+import feedparser
+from datetime import date, datetime, timedelta
+from dateutil import parser
+import pytz
+
+indTz = pytz.timezone("Asia/Kolkata")
+today = datetime.now(indTz)
+current_time = today.strftime("%H:%M:%S")
+
+today = date.today()
+## url
+url1 = 'https://timesofindia.indiatimes.com/rssfeeds/66949542.cms'
+url2 = 'https://feeds.feedburner.com/gadgets360-latest'
+url3 = 'https://feeds.skynews.com/feeds/rss/technology.xml'
+url4 = 'https://www.newscientist.com/section/news/feed/'
+url5 = 'https://www.sciencedaily.com/rss/all.xml'
+url6 = 'https://feeds.washingtonpost.com/rss/business/technology'
+url7 = 'https://www.newscientist.com/feed/home/'
+url8 = 'https://www.techradar.com/rss'
+url9 = 'https://www.wired.com/feed/rss'
+
+rss = []
+
+def convert_to_desired_format(rss_date):
+    # Parse the date using dateutil parser, which can handle both formats
+    dt_object = parser.parse(rss_date)
+
+    # Convert the datetime object to the desired string format
+    formatted_date = dt_object.strftime("%b %d, %Y %H:%M:%S")
+    return formatted_date
+
+
+def find_pubdate_date(pub_date_str):
+    # Parse the pubDate string to a datetime object
+    pub_date = parser.parse(pub_date_str)
+
+    # Get the date portion of the pubDate
+    pub_date_date = pub_date.date()
+
+    return pub_date_date
+
+url_ls = [url1, url2, url3, url4, url5, url6, url7, url8, url9 ]
+scrape_src = ['TOI/TECH', 'NDTV/TECH', 'SKY/TECH', 'NEWSCI/NEWS', 'SCIDAILY/SCI', 'WAPO/TECH', 'NEWSCI/ALL', 'TECHRADAR/TECH', 'WIRED/TECH' ]
+scrape_top = ['SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE' ]
+coun_code = ['GGG', 'GGG', 'GGG', 'GGG', 'GGG', 'GGG', 'GGG', 'GGG', 'GGG']
+tag1 = ['SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE' ]
+tag2 = ['SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE' ]
+tag3 = ['SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE', 'SCIENCE' ]
+
+with open(f"../../scraper/GGGALL/GGGSCIOnce{today.strftime('%d-%m-%Y')}.sql", 'w') as f:
+    for i in range(len(url_ls)):
+        entry = {}
+        entry['url_en'] = url_ls[i]
+        entry['SCRAPE_SOURCE'] = scrape_src[i]
+        entry['SCRAPE_TOPIC'] = scrape_top[i]
+        entry['COUNTRY_CODE'] = coun_code[i]
+        entry['SCRAPE_TAG1'] = tag1[i]
+        entry['SCRAPE_TAG2'] = tag2[i]
+        entry['SCRAPE_TAG3'] = tag3[i]
+        rss.append(entry)
+
+        f1 = feedparser.parse(entry['url_en'])
+        newsItem = f1.entries
+
+        items_to_insert = []
+        for item in newsItem:
+            s = item.summary
+            if len(s) <= 0:
+                s = item.title
+            if len(s) >= 500:
+                s = item.summary[:500]
+            a = item.published
+            published_date = convert_to_desired_format(a)
+            date_only = find_pubdate_date(item.published)
+
+            two_days_ago = today - timedelta(days=2)
+            if date_only <= two_days_ago:
+                continue
+
+            entry_values = [entry['SCRAPE_SOURCE'], entry['SCRAPE_TOPIC'], today.strftime("%Y-%m-%d"),
+                            entry['COUNTRY_CODE'],
+                            entry['SCRAPE_TAG1'], entry['SCRAPE_TAG2'], entry['SCRAPE_TAG3'],
+                            item.title.replace("'", "''"), item.link, published_date, s.replace("'", "''")
+                            , published_date]
+
+            # Join the values with quotes and commas
+            entry_values = ["'" + value + "'" for value in entry_values]
+            items_to_insert.append('(' + ','.join(entry_values).replace('\u2009','').replace('\u20b9','').replace('\u200b','') + ')')
+
+        if items_to_insert:
+            f.write(
+                "INSERT INTO WEB_SCRAPE_RAW(SCRAPE_SOURCE, SCRAPE_TOPIC, SCRAPE_DATE, COUNTRY_CODE, SCRAPE_TAG1, SCRAPE_TAG2,  SCRAPE_TAG3"
+                ", NEWS_HEADLINE, NEWS_URL, NEWS_DTM_RAW, NEWS_EXCERPT, NEWS_DATE) VALUES ")
+            f.write(',\n'.join(items_to_insert))
+            f.write(';\n')

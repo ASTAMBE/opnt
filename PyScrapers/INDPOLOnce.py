38b376202e26e04db0285f314d2bffd8152ba506
astopnt <76823347+astopnt@users.noreply.github.com>
2024-06-02 15:04:47 -0700

    from personal

--- PyScrapers/INDPOLOnce.py
+++ PyScrapers/INDPOLOnce.py
@@ -45,9 +45,9 @@ tag2 = ['POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 
 tag3 = ['POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS']
 ntag = ['PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE']
 
-os.chown('/var/www/html/scraper/INDALL/', 1000,1000)
+# os.chown('/var/www/html/scraper/INDALL/', 1000,1000)
 
-with open(f"/var/www/html/scraper/INDALL/INDPOLOnce{today.strftime('%d-%m-%Y')}.sql", 'w', encoding='utf-8') as f:
+with open(f"INDPOLOnce{today.strftime('%d-%m-%Y')}.sql", 'w', encoding='utf-8') as f:
     for i in range(len(url_ls)):
         entry = {}
         entry['url_en'] = url_ls[i]

103ed0f58872edca843bc54501bcfdab4a1e993a
ASTAMBE <ast.common@gmail.com>
2024-10-19 21:13:47 -0700

    LEN10192024_2

--- pythonProject/localScrape/USAPOLTwice.py
+++ pythonProject/localScrape/USAPOLTwice.py
@@ -43,7 +43,7 @@ tag2 = ['POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 
 tag3 = ['POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS', 'POLITICS']
 ntag = ['PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE', 'PYSCRAPE']
 
-with open(f"/var/www/html/scraper/USAALL/USAPOLTwice{today.strftime('%d-%m-%Y')}.sql", 'w', encoding='utf-8') as f:
+with open(f"c:/AST/scrapefiles/USAPOLTwice{today.strftime('%d-%m-%Y')}.sql", 'w', encoding='utf-8') as f:
     for i in range(len(url_ls)):
         entry = {}
         entry['url_en'] = url_ls[i]

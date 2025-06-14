e225f5d5f75ff516616afd8a7030ad06815e81f2
ASTAMBE <ast.common@gmail.com>
2025-06-14 10:37:46 -0700

    beforeAPILOG

--- opnt_sql/checkusername.sql
+++ opnt_sql/checkusername.sql
@@ -11,8 +11,16 @@ Return 1 if already in use.
 return 0 if available for use.
 
 12/13/2020 AST CONFIRMED
+    06/06/2025 AST: Adding the user bhv log
+    
+    */
 
- */
+/* Adding user action logging portion */
+
+INSERT INTO OPN_USER_BHV_LOG(USERNAME, USERID, USER_UUID, LOGIN_DTM, API_CALL, CONCAT_PARAMS)
+VALUES(uname, null, null, NOW(), 'checkusername', CONCAT('username passed = ', uname));
+
+/* end of user action tracking */
 
 SELECT COUNT(USERNAME) as count FROM OPN_USERLIST WHERE USERNAME= uname;
 
